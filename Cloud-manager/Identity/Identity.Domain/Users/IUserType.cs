98781a9d5eec49b5da48c230f05e2e4c42f30f1b
kent-afk <aterester2@gmail.com>
2026-04-04 14:09:18 +0300

    added user logic

--- Cloud-manager/Identity/Identity.Domain/Users/IUserType.cs
+++ Cloud-manager/Identity/Identity.Domain/Users/IUserType.cs
@@ -2,5 +2,6 @@ namespace Identity.Domain.Users;
 
 public interface IUserType
 {
-    
+    Email.Email Email { get; }
+    HashPassword.HashPassword HashPassword { get; }
 }
---
+++ Cloud-manager/Identity/Identity.Domain/Users/RegisteredUser.cs
@@ -0,0 +1,24 @@
+﻿namespace Identity.Domain.Users;
+
+public class RegisteredUser(Email.Email email, HashPassword.HashPassword hashPassword, Guid userId) : IUserType
+{
+
+    public Email.Email Email { get; private set; } = email;
+    public HashPassword.HashPassword HashPassword { get; private set; } = hashPassword;
+    
+    public Guid UserId { get; } = userId;
+    
+    public void ChangePassword(HashPassword.HashPassword passwordHash)
+    {
+        if (passwordHash == HashPassword)
+        {
+            throw new InvalidOperationException("Passwords do not match.");
+        }
+        HashPassword = passwordHash;
+    }
+    
+    public void ChangeEmail(Email.Email email)
+    { 
+        Email = email;   
+    }
+}
