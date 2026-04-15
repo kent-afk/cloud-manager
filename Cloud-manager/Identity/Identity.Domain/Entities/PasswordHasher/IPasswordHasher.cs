e03a3301286b364fa2d678c2d600f92e9d7d87d5
kent-afk <aterester2@gmail.com>
2026-04-15 15:55:25 +0300

    Add: verify to hasher pass

--- Cloud-manager/Identity/Identity.Domain/Entities/PasswordHasher/IPasswordHasher.cs
+++ Cloud-manager/Identity/Identity.Domain/Entities/PasswordHasher/IPasswordHasher.cs
@@ -2,7 +2,9 @@ using Identity.Domain.ValueObjects.HashPassword;
 
 namespace Identity.Domain.Entities.PasswordHasher;
 
-public interface IPasswordHasher 
+public interface IPasswordHasher
 {
     HashPassword Hash(string password);
+    
+    bool Verify(string providedPassword, string hashedPassword);
 }
--- Cloud-manager/Identity/Identity.Infrastructure/Security/PasswordHasher/PasswordHasher.cs
+++ Cloud-manager/Identity/Identity.Infrastructure/Security/PasswordHasher/PasswordHasher.cs
@@ -7,9 +7,17 @@ public sealed class PasswordHasher : IPasswordHasher
 {
     public HashPassword Hash(string password)
     {
+        if (string.IsNullOrEmpty(password))
+            throw new ArgumentNullException(nameof(password));
+        
         var salt = BCrypt.Net.BCrypt.GenerateSalt();
         var pass = BCrypt.Net.BCrypt.HashPassword(password, salt);
         
         return HashPassword.Create(pass);
+    }
+
+    public bool Verify(string providedPassword, string hashedPassword)
+    {
+        return BCrypt.Net.BCrypt.Verify(providedPassword, hashedPassword);
     }
 }
