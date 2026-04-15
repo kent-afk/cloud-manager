f037fd234d885f50dfc665f4f97d1c87e3135d97
kent-afk <aterester2@gmail.com>
2026-04-15 16:44:03 +0300

    Update: PasswordChangeHandle.cs logic

--- Cloud-manager/Identity/Identity.Application/UseCases/PasswordChange/PasswordChangeHandle.cs
+++ Cloud-manager/Identity/Identity.Application/UseCases/PasswordChange/PasswordChangeHandle.cs
@@ -1,3 +1,5 @@
+using Identity.Domain.Entities.PasswordHasher;
+using Identity.Domain.Entities.UserRepository;
 using Identity.Domain.ValueObjects.Result;
 using MediatR;
 
@@ -5,9 +7,32 @@ namespace Identity.Application.UseCases.PasswordChange;
 
 public class PasswordChangeHandle : IRequestHandler<PasswordChangeCommand, Result<string>>
 {
+    private readonly IUserRepository _userRepository;
+    private readonly IPasswordHasher _passwordHasher;
+    
+    public PasswordChangeHandle(IPasswordHasher passwordHasher, IUserRepository userRepository)
+    {
+        _passwordHasher = passwordHasher;
+        _userRepository = userRepository;
+    }
+
     public async Task<Result<string>> Handle(PasswordChangeCommand request,
         CancellationToken cancellationToken)
     {
-        throw new NotImplementedException();
+        var user = await _userRepository.GetByIdAsync(request.Event.UserId); // ID -> Email 
+        
+        if (user == null)
+            return new Result<string>(false, null, "User not found");
+
+        if (_passwordHasher.Verify(request.Event.NewPassword, user.HashPassword.Value))
+            return new Result<string>(false, null, "Password repeated");
+
+        var newPasswordHash = _passwordHasher.Hash(request.Event.NewPassword);
+        
+        user.ChangePassword(newPasswordHash);
+
+        await _userRepository.UpdateAsync(user);
+        
+        return new Result<string>(true, ErrorMessage:"Password changed");
     }
 }
