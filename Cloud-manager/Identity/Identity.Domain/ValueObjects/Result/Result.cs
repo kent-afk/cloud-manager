4a47658bc7420825f9173ad93d1324cdd24e09c8
kent-afk <aterester2@gmail.com>
2026-04-21 16:53:42 +0300

    Update: Result now support Sucsess/Failure scenarios

--- Cloud-manager/Identity/Identity.Domain/ValueObjects/Result/Result.cs
+++ Cloud-manager/Identity/Identity.Domain/ValueObjects/Result/Result.cs
@@ -1,3 +1,9 @@
 namespace Identity.Domain.ValueObjects.Result;
 
-public record Result<T>(bool Succeeded, T? Value = default, string? ErrorMessage = null);
+public record Result<T>(bool Succeeded, T? Value = default, string? ErrorMessage = null)
+{ 
+    public bool IsFailure => !Succeeded;
+    
+    public static Result<T> Success(T value) => new(true, value);
+    public static Result<T> Failure(string error) => new(false, ErrorMessage: error);
+}
