7446661394e005867b4ac3db67143a96d0e626a8
kent-afk <aterester2@gmail.com>
2026-04-21 16:55:32 +0300

    Deleted: Consumers from identity - only write

--- Cloud-manager/Identity/Identity.Infrastructure/Communicating/ConsumersRabbitMq/IConsumer.cs
+++
@@ -1,8 +0,0 @@
-using RabbitMQ.Client.Events;
-
-namespace Identity.Infrastructure.Communicating.ConsumersRabbitMq;
-
-public interface IConsumer
-{
-    Task ConsumeAsync(BasicDeliverEventArgs @event);
-}
