51d5e4dd08f5b6d70f5baa726a1ee6975825f6aa
kent-afk <aterester2@gmail.com>
2026-04-29 00:51:03 +0300

    Update: Program.cs for Identity microservice

--- Cloud-manager/Identity/Identity.API/Program.cs
+++ Cloud-manager/Identity/Identity.API/Program.cs
@@ -1,8 +1,24 @@
+using Identity.Application;
+using Identity.Infrastructure;
 using Microsoft.AspNetCore.Builder;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddApplication();
+builder.Services.AddInfrastructure(builder.Configuration);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
 
 app.Run();
